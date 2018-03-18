7715d6f6009e840cf51aa0ee7813ef7c27542531
Chris Henry <cdhenry@gmail.com>
2018-03-18 19:02:27 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,18 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+    
+  end
+  
+  get '/figures/new' do
+    
+  end
+
+  get '/figures/:id' do
+    
+  end
+
+  get '/figures/:id/edit' do
+    
+  end
 end
