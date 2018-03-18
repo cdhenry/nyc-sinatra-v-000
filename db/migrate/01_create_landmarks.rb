dd66fe2e11449e896446fe87cf1cc39bbbb18a37
Chris Henry <cdhenry@gmail.com>
2018-03-18 18:24:54 +0000

    automatically backed up by learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,6 @@
 class CreateLandmarks < ActiveRecord::Migration[4.2]
-  raise "Write CreateLandmarks migration here"
+  create_table :landmarks do |t|
+    t.string :name
+    t.integer :year_completed
+  end
 end
