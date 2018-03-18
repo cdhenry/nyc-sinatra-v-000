0ea380e8a6df1cd88d5c18c440d1069f605baf50
Chris Henry <cdhenry@gmail.com>
2018-03-18 18:34:57 +0000

    automatically backed up by learn

--- db/migrate/04_create_figure_titles.rb
+++ db/migrate/04_create_figure_titles.rb
@@ -1,5 +1,5 @@
 class CreateFigureTitles < ActiveRecord::Migration[4.2]
-  create_table :titles do |t|
+  create_table :figure_titles do |t|
     t.integer :title_id
     t.integer :figure_id
   end
