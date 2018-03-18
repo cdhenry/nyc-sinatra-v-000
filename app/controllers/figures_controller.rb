91ae9bfcf95e04897fc1d6828418503c97a17be4
Chris Henry <cdhenry@gmail.com>
2018-03-18 19:44:22 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -33,6 +33,15 @@ class FiguresController < ApplicationController
   end
 
   patch '/figures/:id' do
+    @figure = Figure.create(params[:figure])
+    if !params["title"]["name"].empty?
+     @figure.titles << Title.create(name: params["title"]["name"])
+    end
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+    end
+    @figure.save
+
     redirect to "/figures/#{@figure.id}"
   end
 end
