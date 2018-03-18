5f6f395f0035135c3940facff40002bf066b50e9
Chris Henry <cdhenry@gmail.com>
2018-03-18 18:41:58 +0000

    automatically backed up by learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,5 +1,6 @@
 class CreateLandmarks < ActiveRecord::Migration[4.2]
   create_table :landmarks do |t|
+    t.belongs_to :figure, index: true
     t.string :name
     t.integer :year_completed
   end
