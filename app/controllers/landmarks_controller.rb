8184ab9e30f5d6dbc2b9b7609f72426fc8e71865
Chris Henry <cdhenry@gmail.com>
2018-03-18 19:04:22 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,17 @@
 class LandmarksController < ApplicationController
+  get '/landmarks' do
 
+  end
+
+  get '/figures/new' do
+
+  end
+
+  get '/figures/:id' do
+
+  end
+
+  get '/figures/:id/edit' do
+
+  end
 end
